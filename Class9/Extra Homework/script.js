55ba66bbb6ff6ae4c8f2c135a0dce62a28d4182a
Vlatko Jakimovski <jakimovski_vlatko@yahoo.com>
2021-01-27 13:52:22 +0100

    Commit domasno Class9 plus Extra

---
+++ Class9/Extra Homework/script.js
@@ -0,0 +1,31 @@
+////////  EXERCISE   1   ///////////
+
+// let btn = $("#clicker");
+// let secret = $("#secret");
+
+// btn.click (function() {
+//     // // Fade In
+//     // secret.fadeIn();
+//     // //Slide down
+//     // secret.slideDown(1000);
+
+//     secret.toggle();
+// })
+
+// btn.val('Click me to toggle a hidden secret!');
+
+////////  EXERCISE   2   ///////////
+// let btn = $("#clicker");
+// let paragraphsSecret = $(".secret");
+
+// btn.click (function() {
+//     paragraphsSecret.slideUp(1000);
+// });
+
+////////  EXERCISE   3   ///////////
+let btn = $("#clicker");
+let img = $("img");
+
+btn.click (function() {
+        img.attr('src','womens-sexy-girl-costume-122004.jpg');
+    });
